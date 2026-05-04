6f1cf07dc00f6dde2d6711bc16dee9c9b4410c99
Arif Yahya <arif.yahya@gajicermat.com>
2026-05-04 10:58:59 +0700

    feat: fix initials solution for Moh Arif Yahya

--- 01-basics/solution/05-your-initials.go
+++ 01-basics/solution/05-your-initials.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("MMM")
-	fmt.Println(" MMM")
-	fmt.Println(" MMM")
+	fmt.Println("For the name Moh Arif Yahya...")
+	fmt.Println()
+	fmt.Println("M     M   AAA   Y     Y")
+	fmt.Println("MM   MM  A   A   Y   Y ")
+	fmt.Println("M M M M  A   A    Y Y  ")
+	fmt.Println("M  M  M  AAAAA     Y   ")
+	fmt.Println("M     M  A   A     Y   ")
+	fmt.Println("M     M  A   A     Y   ")
+	fmt.Println("M     M  A   A     Y   ")
 }
